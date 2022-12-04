540f287a0a883200a0f97ae579de0ea76f258ea4
dek <dek@substructure.one>
2022-12-04 12:01:29 -0500

    Begin testing using web workers

--- djangowasm/serve.py
+++ djangowasm/serve.py
@@ -5,7 +5,10 @@ app = flask.Flask(__name__)
 
 @app.route('/static/<path>')
 def static_data(path):
-    return flask.send_from_directory('static', path)
+    response = flask.send_from_directory('static', path)
+    response.headers["Cross-Origin-Opener-Policy"] = "same-origin"
+    response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
+    return response
 
 
 @app.route('/', defaults={'path': ''})
@@ -14,9 +17,18 @@ def catch_all(path):
     # return app.send_static_file('index.html')
     # return app.send_static_file('postgrestest.html')
     # return app.send_static_file('asynctest.html')
-    return flask.render_template(
-        "servedjango.html",
-        package_url='/static/djangosample.zip',
-        settings_package='djangosample.settings',
-        requirements=['django', 'django_postgresql_ws',],
-    )
+    response = app.send_static_file('workertest.html')
+    response.headers["Cross-Origin-Opener-Policy"] = "same-origin"
+    response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
+    # response.headers["Cross-Origin-Resource-Policy"] = "cross-origin"
+    return response
+    # return flask.render_template(
+    #     "servedjango.html",
+    #     package_url='/static/djangosample.zip',
+    #     settings_package='djangosample.settings',
+    #     requirements=[
+    #         'django',
+    #         'django_postgresql_ws',
+    #         'pgwasm==0.2.1',
+    #     ],
+    # )
