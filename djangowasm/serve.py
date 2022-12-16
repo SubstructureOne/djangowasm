1df16a7870290d3485af3664457ede8d991b02a7
dek <dek@substructure.one>
2022-12-16 15:15:29 -0500

    Add endpoint for testing micropip installation

--- djangowasm/serve.py
+++ djangowasm/serve.py
@@ -15,7 +15,7 @@ def static_data(path):
 @app.route('/<path:path>')
 def catch_all(path):
     # return app.send_static_file('index.html')
-    response = app.send_static_file('postgrestest.html')
+    # response = app.send_static_file('postgrestest.html')
     # return app.send_static_file('asynctest.html')
     # response = app.send_static_file('workertest.html')
     response = app.send_static_file('servedjango.html')
@@ -33,3 +33,32 @@ def catch_all(path):
     #         'pgwasm==0.2.1',
     #     ],
     # )
+
+@app.route('/test/micropip/<package>')
+def packageloadtest(package):
+    html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js"></script>
+    <script>
+        async function main() {
+            try {
+                let pyodide = await loadPyodide()
+                await pyodide.loadPackage('micropip')
+                const micropip = pyodide.pyimport('micropip')
+                await micropip.install('PACKAGE_NAME')
+                document.getElementById('result').innerHTML = "Success."
+            } catch (e) {
+                console.log(`Error: ${e}`)
+                document.getElementById('result').innerHTML = `Failure.`
+            }
+        }
+        main().then()
+    </script>
+</head>
+<body>
+    <div id="result" data-cy="result">Loading...</div>
+</body>
+    """
+    return app.make_response(html.replace("PACKAGE_NAME", package))
